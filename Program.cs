f4cf584759d9faa4ec7e3093f09432a6db28314d
Laroub03 <98460971+Laroub03@users.noreply.github.com>
2023-04-18 20:10:30 +0200

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,75 +1,67 @@
-﻿using System;
+using System;
 using System.Threading;
-
 class Program
 {
     static int _number = 60;
-
     static object _lock = new object();
-
-
     static void Main()
     {
+        // Creating two new threads, one for printing stars and one for printing hashes
         Thread starsThread = new Thread(PrintStars);
         Thread hashesThread = new Thread(PrintHashes);
+        // Starting the two threads
         starsThread.Start();
         hashesThread.Start();
+        // Joining the two threads to the main thread
         starsThread.Join();
         hashesThread.Join();
     }
-
+    // Method to print stars
     static void PrintStars()
     {
+        while (true)
         {
-            while (true)
+            Monitor.Enter(_lock);
+            try
             {
-                Monitor.Enter(_lock);
-
-                try
+                // Looping through the number of stars to print
+                for (int i = 0; i < _number; i++)
                 {
-                    for (int i = 0; i < _number; i++)
-                    {
                     Console.Write("*");
-                    }
-                    Console.Write(_number);
-                    Console.WriteLine();
-                    _number += 60;
-                    Thread.Sleep(1000);
                 }
-                finally
-                {
-                    Monitor.Exit(_lock);
-
-                }
+                Console.Write(_number);
+                Console.WriteLine();
+                _number += 60;
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
             }
         }
     }
-
+    // Method to print hashes
     static void PrintHashes()
     {
+        while (true)
         {
-            while (true)
+            Monitor.Enter(_lock);
+            try
             {
-                Monitor.Enter(_lock);
-
-                try
+                // Looping through the number of hashes to print
+                for (int i = 0; i < _number; i++)
                 {
-                    for (int i = 0; i < _number; i++)
-                    {
-                        Console.Write("#");
-                    }
-                    Console.Write(_number);
-                    Console.WriteLine();
-                    _number += 60;
-                    Thread.Sleep(1000);
+                    Console.Write("#");
                 }
-                finally
-                {
-                    Monitor.Exit(_lock);
-
-                }
+                Console.Write(_number);
+                Console.WriteLine();
+                _number += 60;
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
             }
         }
     }
 }
-
